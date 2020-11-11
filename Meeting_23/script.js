0c79524777be7701d57e446f9ea475583ff019ed
charley-rantung <rantungcharley@gmail.com>
2020-11-11 14:32:29 +0800

    Exercise 7

--- Meeting_23/script.js
+++ Meeting_23/script.js
@@ -1,2 +1,7 @@
 let berat = prompt("Masukkan berat anda (kg): ");
-let tinggi = prompt("Masukkan tinggi anda (cm): ");
+let tinggi = prompt("Masukkan tinggi anda (meter): ");
+
+let bmi = berat / (tinggi * tinggi);
+
+console.log(`BMI = ${bmi}`);
+alert(`BMI = ${bmi}`);
